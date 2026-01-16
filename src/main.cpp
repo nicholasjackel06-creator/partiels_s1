46eab7cdad0bedd04e699fbef014fd49ab2d8881
nicholasjackel06-creator <nicholasjackel06@gmail.com>
2026-01-16 10:26:05 +0000

    Recuperation du choix de l'utilisateur

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,22 @@ int main() {
   int velosDispos = 20;
   int capacite = 0;
   int choix;
-
+ 
   cout << "******Gestion d'une station Velo ******"<< endl << endl;
   cout << "----------------------------------------"<< endl << endl;
   cout << "Velos disponibles :"<< velosDispos << endl;
-  cout << "Points d'attache disponibles"<< capacite << endl;
-
+  cout << "Points d'attache disponibles: "<< capacite << endl;
+  
 
   cout << "_________________________________________"<< endl << endl;
   cout << "1. Emprunter un Velo" << endl ;
   cout << "2. Restituer un Velo" << endl;
   cout << "3. Estimer le cout de la location" << endl;
   cout << "4. Quitter l'application" << endl << endl;
+  
+  cout << "faire un choix :" << endl;
+  cin >> choix;
+  cout <<"vous avez choisi l'option"<< choix << endl;
 
   return 0;
 
