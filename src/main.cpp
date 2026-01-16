c3b3fb880eb22856a8c31530736521ce4d45beb4
nicholasjackel06-creator <nicholasjackel06@gmail.com>
2026-01-16 11:23:20 +0000

    dernier version du code Non fonctionnel

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,13 @@ int main() {
   const float coutHoraireMeca = 4.0;
   const float coutHoraireElec = 5.0;
   int velosDispos = 20;
-  int capacite = 0;
-  int choix;
- 
+  int capacite = 20;
+  int choix ;
+  int min;
   cout << "******Gestion d'une station Velo ******"<< endl << endl;
   cout << "----------------------------------------"<< endl << endl;
   cout << "Velos disponibles :"<< velosDispos << endl;
-  cout << "Points d'attache disponibles: "<< capacite << endl;
+  cout << "Points d'attache disponibles:  "<< capacite - velosDispos << endl;
   
 
   cout << "_________________________________________"<< endl << endl;
@@ -19,6 +19,26 @@ int main() {
   cout << "2. Restituer un Velo" << endl;
   cout << "3. Estimer le cout de la location" << endl;
   cout << "4. Quitter l'application" << endl << endl;
+
+  switch(choix) {
+    case '0':
+    cout << "on decremente le nombre de velos disponibles \n";
+      break;
+    case '1':
+    cout<< "on incremente le nombre de velos disponibles \n ";
+      break;
+    case '2':
+    cout << "Duree en minutes  ";
+      cin >> min;
+    cout << "Vous souhaitez conserver le velos? ";
+    cout << "Velos Mecanique ou electrique ?";
+      
+      break;
+    case '3':
+    cout << " vous avez choisi l'option: Quitter l'application";
+      break;
+    
+  }
   
   cout << "faire un choix :" << endl;
   cin >> choix;
