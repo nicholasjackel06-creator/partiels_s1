ec19a368c2d2d4a415758eaa57cc769a99bbd719
nicholasjackel06-creator <nicholasjackel06@gmail.com>
2026-01-16 10:12:06 +0000

    ajout de l'affichage des disponiblites

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+  const float coutHoraireMeca = 4.0;
+  const float coutHoraireElec = 5.0;
+  int velosDispos = 20;
+  int capacite = 0;
+  int choix;
+
+  cout << "******Gestion d'une station Velo ******"<< endl << endl;
+  cout << "----------------------------------------"<< endl << endl;
+  cout << "Velos disponibles :"<< velosDispos << endl;
+  cout << "Points d'attache disponibles"<< capacite << endl;
+
+
+  cout << "_________________________________________"<< endl << endl;
+  cout << "1. Emprunter un Velo" << endl ;
+  cout << "2. Restituer un Velo" << endl;
+  cout << "3. Estimer le cout de la location" << endl;
+  cout << "4. Quitter l'application" << endl << endl;
+
+  return 0;
+
+    }
